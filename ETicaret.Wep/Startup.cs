143d71070e49c96d9b57ecc6a0ca9ecda9d8d96d
bünyamin macit <macitbunyamin@gmail.com>
2019-03-06 00:19:38 +0300

    sdf

---
+++ ETicaret.Wep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETicaret.Wep.Startup))]
+namespace ETicaret.Wep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
